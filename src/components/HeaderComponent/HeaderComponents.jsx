e62f5e9eaf6b0adcaacd4fb2089f6e10c819cd02
thelij2000 <80305593+thelij2000@users.noreply.github.com>
2024-03-20 21:29:49 +0700

    p3

--- src/components/HeaderComponent/HeaderComponents.jsx
+++ src/components/HeaderComponent/HeaderComponents.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Col } from "antd";
+import { WrapperHeaderAccount, WrapperHeader, WrapperTextHeader, WrapperTextHeader12 } from "./style";
+import Search from "antd/es/transfer/search";
+import { UserOutlined, CaretDownOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 
 const HeaderComponent = () => {
     return (
-        <div>HeaderComponent</div>
-    )
-}
+        <WrapperHeader gutter={16}>
+            <Col span={6}>
+                <WrapperTextHeader>
+                    Nhom23
+                </WrapperTextHeader>
+            </Col>
+            <Col span={12}>
+                <Search
+                    placeholder="input search text"
+                    enterbutton
+                />
+            </Col>
+            <Col span={6} style={{display: 'flex', gap: '20px'}}>
+                <WrapperHeaderAccount>
+                    <UserOutlined style={{ fontSize: '30px' }} />
+                    <div><WrapperTextHeader12>Đăng nhập/Đăng ký</WrapperTextHeader12>
+                        <div>
+                        <WrapperTextHeader12>Tài khoản</WrapperTextHeader12>
+                        <CaretDownOutlined />
+                        </div>
+                    </div>
+                </WrapperHeaderAccount>
+                <div>
+                    <div>
+                        <ShoppingCartOutlined style={{ fontSize: '30px', color: 'white' }} /> {/* Set color to white */}
+                        <WrapperTextHeader12>Giỏ hàng</WrapperTextHeader12>
+                    </div>
+                </div>
+            </Col>
+        </WrapperHeader>
+    );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
---
+++ src/components/HeaderComponent/style.js
@@ -0,0 +1,24 @@
+import { Row } from "antd";
+import styled from "styled-components";
+
+export const WrapperHeader = styled(Row)`
+    padding: 10px 120px;
+    background-color: rgb(26, 148, 255)
+`
+export const WrapperTextHeader = styled.span`
+    font-size: 18px;
+    color: #fff;
+    font-weight: bold;
+    text-align: left
+`
+
+export const WrapperHeaderAccount = styled.div`
+    display: flex;
+    aglin-items: center;
+    color: #fff;
+    font-size: 12px;
+`
+export const WrapperTextHeader12 = styled.span`
+    font-size: 12px;
+    color: #fff;
+`
